9535d6a4e9893e1d4f1827abf61d82881884b021
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-11-17 23:50:01 +0530

    commit

--- src/components/dashboard/DashboardForm.jsx
+++ src/components/dashboard/DashboardForm.jsx
@@ -16,7 +16,20 @@ const DashboardForm = () => {
       <div className={styles.inputField}>
         <p>Custom song request amount-</p>
         <div className={styles.inputOptions}>
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="444"
+            className={styles.textInputField}
+          />
+        </div>
+      </div>
+      <div className={styles.inputField}>
+        <p>Regular song request amounts, from high to low-</p>
+        <div className={styles.inputOptions}>
+          <input type="button" value="199" className={styles.textInputField} />
+          <input type="button" value="149" className={styles.textInputField} />
+          <input type="button" value="99" className={styles.textInputField} />
+          <input type="button" value="49" className={styles.textInputField} />
         </div>
       </div>
     </form>
